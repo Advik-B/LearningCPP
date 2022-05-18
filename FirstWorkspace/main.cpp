7ef6d8e396874596979f037aa709e70dfcea5fe9
Advik <advik.b@gmail.com>
2022-05-18 20:28:02 +0530

    Create main.cpp

---
+++ FirstWorkspace/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main () {
+    // cout means console-out
+    // you know what endl means (\n)
+    // I STILL dont know what the "<<" means
+    std::cout << "Hello Testing"  << std::endl;
+    return 0;
+}
