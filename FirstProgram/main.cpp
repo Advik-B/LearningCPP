c6c1d0fff71d4a7dc885d783bc081adf2203c3c1
Advik <advik.b@gmail.com>
2022-05-18 23:21:17 +0530

    cmake shit

---
+++ FirstProgram/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    int fav_number;
+    // int real_fav_number = 72;
+    std::cout << "Enter your favorate between number 1 and a hundred (1-100): ";
+    std::cin >> fav_number;
+    std::cout << "Amazing! that is my favorate number too. (" << fav_number << ")" << std::endl;
+    return 0;
+
+}
