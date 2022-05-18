4e1b3bf3ef31a6a8406c6962113d5b629300921e
Advik <advik.b@gmail.com>
2022-05-18 23:20:48 +0530

    Update main.cpp

--- FirstWorkspace/main.cpp
+++ FirstWorkspace/main.cpp
@@ -4,6 +4,6 @@ int main () {
     // cout means console-out
     // you know what endl means (\n)
     // I STILL dont know what the "<<" means
-    std::cout << "Hello Testing"  << std::endl;
+    std::cout << "Hello Testing" << std::endl;
     return 0;
 }
