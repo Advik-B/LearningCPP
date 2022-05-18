08b89105f307c753388296c4900aa73dd6db5bc3
Advik <advik.b@gmail.com>
2022-05-18 22:30:33 +0530

    Create run.py

---
+++ FirstWorkspace/run.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+import sys
+
+if len(sys.argv) < 2:
+    print(
+        """USAGE: 
+    %s: file-to-compile
+        """ % __file__.split("\\")[-1]
+    )
+
+    sys.exit(1)
+
+COMPILER = "g++"
+WARNINGS = "all"
+STANDERD = "c++14"
+OUTPUT = "%(-)%.exe"
+DELETEAFTER = True
+
+fname = OUTPUT.replace("%(-)%", sys.argv[1])
+
+subprocess.run([COMPILER, f"-W{WARNINGS}", f"-std={STANDERD}", sys.argv[1], "-o", fname], shell=True)
+
+subprocess.run(fname, shell=True)
+
+if DELETEAFTER:
+    try:
+        os.remove(fname)
+    except FileNotFoundError:
+        pass
