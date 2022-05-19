b50bfb02bcf32330f85bd48e897e0daca6987bfb
Advik <advik.b@gmail.com>
2022-05-19 18:06:46 +0530

    Who Let the Bugs Out??

---
+++ Challange1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int number;
+    std::cout << "Enter any number: ";
+    std::cin >> number;
+    std::cout << "Amazing! that is my favorate number too!" << std::endl;
+    std::cout << "No, really. " << number << " Is my favorate number" << std::endl;
+    return 0;
+}
