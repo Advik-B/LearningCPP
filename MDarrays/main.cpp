f9d1fa98e7b0af001348cb3e99c73f68c7c6feba
Advik <advik.b@gmail.com>
2022-05-20 16:37:31 +0530

    I honestly wish I could remember what was going on here...

---
+++ MDarrays/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int movie_rating [3][4]
+    {
+            {0, 4, 3, 5},
+            {2, 4, 6, 8},
+            {10, 20, 30, 40}
+    };
+    // std::cout << movie_rating; Lol this does not even work
+    return 0;
+}
