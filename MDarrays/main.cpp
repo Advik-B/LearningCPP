604d4bac480270e07175a41da455445dacfb60c2
Advik <advik.b@gmail.com>
2022-05-28 16:09:08 +0530

    Update main.cpp

--- MDarrays/main.cpp
+++ MDarrays/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+using namespace std;
 int main() {
     int movie_rating [3][4]
     {
@@ -8,5 +9,6 @@ int main() {
             {10, 20, 30, 40}
     };
     // std::cout << movie_rating; Lol this does not even work
+    cout << movie_rating[0][2] << endl;
     return 0;
 }
