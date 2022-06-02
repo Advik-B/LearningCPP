ab1413fe0b4b30b742a05cb8041ce00f25b316be
Advik <advik.b@gmail.com>
2022-06-02 15:38:29 +0530

    changes my dud

---
+++ USD-INR/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+    const double usd {77.61};
+    cout << "Convert USD to INR" << endl;
+    cout << "(Dollars to rupees)" << endl;
+    cout << "Enter the amount in USD: ";
+    return 0;
+}
