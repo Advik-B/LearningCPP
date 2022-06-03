10019071fa5e5dfa9e9e260d1641bac87ed26355
Advik <advik.b@gmail.com>
2022-06-03 05:50:27 +0530

    Update main.cpp

--- USD-INR/main.cpp
+++ USD-INR/main.cpp
@@ -6,8 +6,11 @@ using std::endl;
 
 int main() {
     const double usd {77.61};
+    double inr {0};
     cout << "Convert USD to INR" << endl;
     cout << "(Dollars to rupees)" << endl;
     cout << "Enter the amount in USD: ";
+    cin >> inr;
+    cout << "The amount in INR is: " << inr * usd << "/-" <<  endl;
     return 0;
 }
