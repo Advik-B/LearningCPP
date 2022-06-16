ba0ee255caa35157568552520dbaa5cf5239c7c6
Advik <advik.b@gmail.com>
2022-06-16 17:33:29 +0530

    ad

---
+++ OperatorPrecedence/OperatorPrecedence.cpp
@@ -0,0 +1,14 @@
+﻿// OperatorPrecedence.cpp : Defines the entry point for the application.
+//
+
+#include "OperatorPrecedence.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	custom_namespace::say_hello("World");
+	return 0;
+}									
---
+++ OperatorPrecedence/OperatorPrecedence.h
@@ -0,0 +1,13 @@
+﻿// OperatorPrecedence.h : Include file for standard system include files,
+// or project specific include files.
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+namespace custom_namespace
+{	
+	using namespace std;
+	void say_hello(string name)
+	{
+		cout << "Hello " << name << endl;
+	}
+}
