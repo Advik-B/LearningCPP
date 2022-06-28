fe8952a76198456c24c71a6070d16711b7c13447
Advik <advik.b@gmail.com>
2022-06-28 19:29:51 +0530

    changes

--- IfStatement/main.cpp
+++ IfStatement/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <math.h>
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    using namespace std;
+	cout << 
+    "Welcome to the c++ control flow\n" <<
+	"This program will take a number and check if the number is even or odd\n" <<
+    endl;
+	long long number{0};
+	cout << "Enter a number: ";
+	cin >> number;
+	if (fmod(number, 2) == 0) {
+		cout << "The number is even" << endl;
+	}
+	else {
+		cout << "The number is odd" << endl;
+	}
     return 0;
 }
