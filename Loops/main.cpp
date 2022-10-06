67c1b4ec7412cad39ea79a9cbf219e1312d3d01b
Advik <advik.b@gmail.com>
2022-10-06 15:22:55 +0530

    🥴😖

---
+++ Loops/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> numbers{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+	// For-loop (range)
+	for (int num : numbers) {
+		cout << num << "\n";
+	};
+
+	// For-loop (classic)
+	for (int i{ 0 }; i <= 10; i++) {
+
+	};
+	
+	return 0;
+}
