21eadaaeadaa0684faee3a372d2552c89e565e56
Advik <advik.b@gmail.com>
2022-10-08 18:24:00 +0530

    Assignment done!

--- RangeBased-Assignment/main.cpp
+++ RangeBased-Assignment/main.cpp
@@ -1,20 +1,31 @@
 #include <vector>
+#include <iostream>
+
 using namespace std;
 
 int count_divisible() {
     
     vector<int> vec {1,3,5,15,16,17,18,19,20,21,25,26,27,30,50,55,56,58,100,200,300,400,500,600,700};
     int count{ 0 };
+    int current{ 0 };
     //---- WRITE YOUR CODE BELOW THIS LINE----
     
+    for (int num: vec){
     
+        if ((num % 3 == 0) || (num % 5 == 0))
+            {
+                count++;
+            };
+    };
     //---- WRITE YOUR CODE ABOVE THIS LINE----
     //---- DO NOT CHANGE THE CODE BELOW THIS LINE----
     return count;
-}
+};
 
 int main()
 {
     cout << count_divisible();
     return 0;
-}
+};
+
+//int main(){return 0;}
