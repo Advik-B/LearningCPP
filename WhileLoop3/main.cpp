65443fd90df41a276746d7a23113deecab29c2c4
Advik <advik.b@gmail.com>
2022-11-13 12:02:34 +0530

    Hwello I am back!

--- WhileLoop3/main.cpp
+++ WhileLoop3/main.cpp
@@ -2,16 +2,15 @@
 
 // This was a waste of time 🙁
 using namespace std;
-int main()
-{
+
+int main() {
     long long stop { 0 };
-    long long current{0};
+    long long current { 0 };
     cout << "Enter a number to count up to: ";
     cin >> stop;
-    while (current >= stop){
+    while (current <= stop){
         cout << current << "\n";
-        stop--;
+        current++;
     }
-    
-    
+    return 0;
 }
