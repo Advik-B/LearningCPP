60c1d22361022352f9cc462769ec10db911373d9
Advik <advik.b@gmail.com>
2022-11-22 22:54:01 +0530

    Update main.cpp

--- The-Qt-Project/main.cpp
+++ The-Qt-Project/main.cpp
@@ -1,6 +1,9 @@
 #include <QApplication>
+#include "window.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    MainWindow UI;
+    UI.show();
     return QApplication::exec();
 }
