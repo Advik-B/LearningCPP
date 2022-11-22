5028eb957e642e0102042e3d8b04ca4a32f5d647
Advik <advik.b@gmail.com>
2022-11-22 23:23:56 +0530

    Update main.cpp

--- The-Qt-Project/main.cpp
+++ The-Qt-Project/main.cpp
@@ -1,9 +1,13 @@
 #include <QApplication>
 #include "window.h"
+#include "io_util.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow UI;
     UI.show();
+    util_io::Print("Hello World!");
+    util_io::Print(123);
+    util_io::FileWrite("test.txt", "Hello World!");
     return QApplication::exec();
 }
