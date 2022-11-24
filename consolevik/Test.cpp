1e17031131dbdeb2279a3eab6b6969e977fd4c36
Advik <advik.b@gmail.com>
2022-11-24 19:32:18 +0530

    Update Test.cpp

--- consolevik/Test.cpp
+++ consolevik/Test.cpp
@@ -1,3 +1,13 @@
 //
 // Created by Advik on 23/11/22.
 //
+#include "colors.cpp"
+#include <iostream>
+
+using namespace std;
+using namespace colors;
+int main()
+{
+    cout << dye("Hello World", "red") << endl;
+    return 0;
+}
