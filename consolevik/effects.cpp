ea362f781876655347ebb07937545edc61b82ea3
Advik <advik.b@gmail.com>
2022-11-24 19:36:33 +0530

    Create effects.cpp

---
+++ consolevik/effects.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+namespace effects {
+
+    using std::string;
+
+    const string UNDERLINE = "\033[1m";
+    const string BOLD = "\033[4m";
+    const string REVERSE = "\033[7m";
+    const string RESET = "\033[0m";
+    const string BLINK = "\033[5m";
+    const string INVISIBLE = "\033[8m";
+
+}
