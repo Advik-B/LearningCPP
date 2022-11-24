1aa92bae5f951942354c459317628d033c275110
Advik <advik.b@gmail.com>
2022-11-24 19:50:45 +0530

    Fix swapped colors

--- consolevik/effects.cpp
+++ consolevik/effects.cpp
@@ -4,8 +4,8 @@ namespace effects {
 
     using std::string;
 
-    const string UNDERLINE = "\033[1m";
-    const string BOLD = "\033[4m";
+    const string UNDERLINE = "\033[4m";
+    const string BOLD = "\033[1m";
     const string REVERSE = "\033[7m";
     const string RESET = "\033[0m";
     const string BLINK = "\033[5m";
