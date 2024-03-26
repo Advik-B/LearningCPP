49773fc38cef5e9e8d43c38839d7cdf49db19e35
Advik <advik.b@gmail.com>
2024-03-26 22:52:01 +0530

    Update main2.cpp

--- Section10-2/main2.cpp
+++ Section10-2/main2.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
+
 int main() {
-    string s1;
-    cout << "Enter a string: ";
-    getline(cin, s1);
-    cout << "You entered: " << s1 << "\n";
+    string journal_entry_1 {"Isaac Newton"};
+    string journal_entry_2 {"Leibniz"};
+
+    //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    //----WRITE YOUR CODE BELOW THIS LINE----
+
+    journal_entry_1.erase(0, 6);
+
+    if (journal_entry_2 < journal_entry_1)
+        journal_entry_2.swap(journal_entry_1);
+
+    /*THE FOLLOWING WILL ALSO WORK
+    if (journal_entry_1 > journal_entry_2)
+        journal_entry_1.swap(journal_entry_2);
+    */
+
+    //----WRITE YOUR CODE ABOVE THIS LINE----
+    //----DO NOT MODIFY THE CODE BELOW THIS LINE----
+
+    cout << journal_entry_1 << "\n" << journal_entry_2;
     return 0;
 }
