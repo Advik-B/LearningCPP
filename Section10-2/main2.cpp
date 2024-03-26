d676d80e7b3f9645e68551b0b2bda2cd49aca21f
Advik <advik.b@gmail.com>
2024-03-26 22:11:23 +0530

    Update main2.cpp

--- Section10-2/main2.cpp
+++ Section10-2/main2.cpp
@@ -1,3 +1,11 @@
-//
-// Created by Advik on 26-03-2024.
-//
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+    string s1;
+    cout << "Enter a string: ";
+    getline(cin, s1);
+    cout << "You entered: " << s1 << "\n";
+    return 0;
+}
