b61d4059a6f69decfc4845b01fb75e338236c64f
Advik <advik.b@gmail.com>
2024-06-13 13:25:41 +0530

    Add section 10 challenge

--- Section10-LetterPyramid/main.cpp
+++ Section10-LetterPyramid/main.cpp
@@ -4,6 +4,21 @@
 
 using namespace std;
 int main() {
-    cout << UINT64_MAX << endl;
+    string input;
+    cout << "Enter a string: ";
+    getline(cin, input);
+    for (int i = 0; i < input.length(); i++) {
+        for (int j = 0; j < input.length() - i - 1; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++) {
+            cout << input[j];
+        }
+        for (int j = i - 1; j >= 0; j--) {
+            cout << input[j];
+        }
+        cout << endl;
+    }
+
     return 0;
 }
